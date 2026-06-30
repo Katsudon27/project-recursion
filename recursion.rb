63d9eb4f55e6172df2b20fa6680233f5cc5f58e5
Katsudon27 <katsudon1127@gmail.com>
2026-06-30 19:18:08 +1000

    Add implementation for #merge_sort

--- recursion.rb
+++ recursion.rb
@@ -27,3 +27,36 @@ def fibs_rec(num)
     return result
   end
 end
+
+def merge_sort(array)
+  if array.length < 2
+    return array
+  else
+    result = []
+    split_array = array.each_slice( (array.size/2.0).round ).to_a
+    left_array = merge_sort(split_array[0])
+    right_array = merge_sort(split_array[1])
+    
+    i, j = 0, 0
+    while i < left_array.length && j < right_array.length
+      if left_array[i] < right_array[j]
+        result << left_array[i]
+        i += 1
+      else 
+        result << right_array[j]
+        j += 1
+      end
+    end
+
+    result += left_array.drop(i)
+    result += right_array.drop(j)
+    
+    result
+  end
+end
+
+p merge_sort([])
+p merge_sort([73]) 
+p merge_sort([1, 2, 3, 4, 5]) 
+p merge_sort([3, 2, 1, 13, 8, 5, 0, 1])
+p merge_sort([105, 79, 100, 110])
